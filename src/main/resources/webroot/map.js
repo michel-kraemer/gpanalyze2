1137706332b5ba46f043d015fa355f32657bf601
Michel Kraemer <michel@spamihilator.com>
2015-07-24 20:21:13 +0200

    Save map state in url

--- src/main/resources/webroot/map.js
+++ src/main/resources/webroot/map.js
@@ -2,6 +2,7 @@ angular.module("map", ["trackservice"])
 
 .controller("MapCtrl", function($scope, $timeout, TrackService) {
   var trackPolylines = {};
+  var doUpdateState = true;
   
   // initialize map
   var map = L.map('map', { zoomControl: false }).fitWorld();
@@ -10,10 +11,29 @@ angular.module("map", ["trackservice"])
     minZoom: 3
   }).addTo(map);
   
-  // move to current position (but keep min zoom level)
-  map.locate({
-    setView: true,
-    maxZoom: 3
+  var loadState = function(state) {
+    if (state) {
+      map.setView(state.center, state.zoom);
+    } else {
+      var re = /@([0-9.]+),([0-9.]+),([0-9]+)/;
+      var match = re.exec(location.hash);
+      if (match) {
+        map.setView(L.latLng(match[1], match[2]), match[3]);
+      } else {
+        // move to current position (but keep min zoom level)
+        map.locate({
+          setView: true,
+          maxZoom: 3
+        });
+      }
+    }
+  };
+  loadState(history.state);
+  
+  $(window).bind("popstate", function(e) {
+    var s = e.originalEvent.state;
+    doUpdateState = false;
+    loadState(s);
   });
   
   // handle zoom buttons
@@ -34,12 +54,45 @@ angular.module("map", ["trackservice"])
     $scope.zoomOutDisabled = map.getZoom() == map.getMinZoom();
   };
   
+  var makeQueryString = function() {
+    var qs = "@" + map.getCenter().lat + "," + map.getCenter().lng + "," + map.getZoom();
+    var result;
+    var re = /(.*?)@[^\/]+(.*)/;
+    var match = re.exec(location.hash);
+    if (match) {
+      result = match[1] + qs + match[2];
+    } else {
+      result = location.pathname;
+      if (result[result.length - 1] != "/") {
+        result = result + "/";
+      }
+      result += qs;
+    }
+    if (result[0] != '#') {
+      result = "#" + result;
+    }
+    return result;
+  };
+  
+  var updateState = function() {
+    if (doUpdateState) {
+      history.pushState({zoom: map.getZoom(), center: map.getCenter()}, "", makeQueryString());
+    } else {
+      doUpdateState = true;
+    }
+  };
+  
   map.on("zoomend", function() {
     // update zoom button state on every zoom
     $timeout(function() {
       updateZoomDisabled();
     }, 0);
+    updateState();
   });
+  
+  map.on("dragend", function() {
+    updateState();
+  })
   
   // add track listener
   var trackListener = {
